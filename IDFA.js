9e1743abf42e66ff27c03670a615aabc0ff84468
Alan Keiss <alankeiss@MacBook-Pro-de-Alan.local>
2020-04-06 13:58:58 +0200

    ios get idfv function

--- IDFA.js
+++ IDFA.js
@@ -1,12 +1,28 @@
 // @flow
 
-import { NativeModules } from 'react-native';
+import { NativeModules, Platform } from 'react-native';
 
 const { PTRIDFA } = NativeModules;
 
 class IDFA {
+  async getAdvertiserID(): Promise<string> {
+    if (Platform.OS === "ios") {
+      const idfa = await PTRIDFA.getIDFA();
+      if (idfa.length > 0)
+        return new Promise(resolve => resolve(idfa));
+      else
+        return PTRIDFA.getIDFV();
+    }
+    else
+      return PTRIDFA.getIDFA();
+  }
+
   getIDFA(): Promise<string> {
     return PTRIDFA.getIDFA();
+  }
+
+  getIDFV(): Promise<string> {
+    return Platform.OS === "ios" ? PTRIDFA.getIDFV() : new Promise(resolve => resolve(""));
   }
 }
 
--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,7 @@
 declare module 'react-native-idfa' {
   export namespace IDFA {
+    export function getAdvertiserID(): Promise<string>;
     export function getIDFA(): Promise<string>;
+    export function getIDFV(): Promise<string>;
   }
 }
